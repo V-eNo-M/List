99defb9eeffb6240281b38ffb14f77d7c7a0e053
VeNoM <venom.cod@yandex.ru>
2016-03-13 01:45:39 +0300

    Final

--- src/List.java
+++ src/List.java
@@ -1,5 +1,6 @@
 /**
- * Created by 1 on 12.03.2016.
+ * Created by Ivan Shulga on 12.03.2016.
+ * Задача 1. Циклический список
  */
 public class List {
     private int value;
--- src/Solver.java
+++ src/Solver.java
@@ -1,3 +1,7 @@
+/**
+ * Created by Ivan Shulga on 12.03.2016.
+ * Задача 1. Циклический список
+ */
 public class Solver {
 
     public static void main(String[] args) {
